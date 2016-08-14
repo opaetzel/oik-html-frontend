92de34ef7f041a3e7072147acebdf00311a8d0ad
Oliver Paetzel <oliver.paetzel@gmx.net>
2016-08-14 22:37:28 +0200

    add index route and bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,12 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js',{
+	destDir: 'assets'
+  });
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css',{
+	destDir: 'assets'
+  });
 
   return app.toTree();
 };
