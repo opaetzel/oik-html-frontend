93799ea6f585d924dba256b2a2c0306248acb2a2
Oliver Paetzel <oliver.paetzel@gmx.net>
2016-08-15 13:59:54 +0200

    add link to landing page and fix some less

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     activate: function(){
-        Ember.$('html').toggleClass("full");
+        Ember.$('html,body').toggleClass("full");
         console.log("entering index")
     },
     deactivate: function(){
-        Ember.$('html').toggleClass("full");
+        Ember.$('html,body').toggleClass("full");
     }
 });
