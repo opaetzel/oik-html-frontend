7d2e6bc210e6de6fc4f5e270f212fe7fb1b4431b
Oliver Paetzel <oliver.paetzel@gmx.net>
2016-08-18 14:06:35 +0200

    get affix on page.hbs to work properly

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,9 +19,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/bootstrap/dist/js/bootstrap.js',{
-	destDir: 'assets'
-  });
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
   app.import(app.bowerDirectory + '/bootstrap/fonts/glyphicons-halflings-regular.eot', {
           destDir: 'fonts'
   });
