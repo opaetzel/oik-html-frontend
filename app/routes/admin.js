0295a237531855cbf7bac90534c57269ee1866c8
Oliver Paetzel <oliver.paetzel@intranda.com>
2016-09-06 16:45:25 +0200

    secure admin route

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    currentUser: Ember.inject.service('current-user'),
+    beforeModel() {
+        console.log(this.get('currentUser.user.groups').indexOf('admin'));
+        if(!(this.get('currentUser.user.groups').indexOf('admin') > -1)) {
+            this.transitionTo('index');
+        }
+    },
     model() {
         return Ember.RSVP.hash({
            users: this.store.findAll('user'),
