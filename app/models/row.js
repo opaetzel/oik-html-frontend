957664a275344c60b84dfd5d49a4750f5145b377
Oliver Paetzel <oliver.paetzel@intranda.com>
2016-09-30 15:21:08 +0200

    update row datamodel for more flexibility

--- app/models/row.js
+++ app/models/row.js
@@ -5,6 +5,10 @@ import { belongsTo } from 'ember-data/relationships';
 export default DS.Model.extend({
     left_markdown: attr(),
     right_markdown: attr(),
+    left_has_image: attr(),
+    right_has_image: attr(),
     leftImage: belongsTo('image'),
-    rightImage: belongsTo('image')
+    rightImage: belongsTo('image'),
+    left_is_argument: attr(),
+    right_is_argument: attr()
 });
