42a5b6614926049ed667ec42ed9bc30bfe0ec7de
Oliver Paetzel <oliver.paetzel@gmx.net>
2016-10-20 13:22:28 +0200

    add saving functionality for units to admin page

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -6,6 +6,11 @@ export default Ember.Controller.extend({
             this.get('model.users').forEach(function(item) {
                 item.save();
             });
+        },
+        saveUsers: function() {
+            this.get('model.units').forEach(function(item) {
+                item.save();
+            });
         }
     }
 });
