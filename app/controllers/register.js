d57b1ea5680c7f56ae1028fbd89978f7255ee469
Oliver Paetzel <oliver.paetzel@gmx.net>
2016-10-21 08:31:45 +0200

    better error messages for register view

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -4,14 +4,19 @@ export default Ember.Controller.extend({
     actions: {
         register: function() {
             this.set('error', false);
+            this.set('errormessage', null);
             let credentials = this.getProperties('username', 'email', 'password');
             console.log(credentials);
             let newUser = this.get('store').createRecord('newuser', credentials);
             console.log(credentials);
             newUser.save().then( () => {
                 this.set('sent', true);
-            }).catch( () => {
-                this.set('error', true);
+            }).catch( (error) => {
+                if(error.errors[0].status === "409") {
+                    this.set('errormessage', "Username bereits vergeben");
+                } else {
+                    this.set('error', true);
+                }
             });
         },
         newUser: function() {
