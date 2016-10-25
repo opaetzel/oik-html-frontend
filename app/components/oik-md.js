68ad2e10f47df3912422a15882cfe5af313310c6
Oliver Paetzel <oliver.paetzel@gmx.net>
2016-10-26 00:02:51 +0200

    add styling to edit-page previews; add images to edit-page previews; fix footnote-button

--- app/components/oik-md.js
+++ app/components/oik-md.js
@@ -1,19 +1,86 @@
 import Ember from 'ember';
+import Remarkable from 'remarkable';
 import EmberRemarkableComponent from 'ember-remarkable/components/md-text';
 
 const {computed} = Ember;
 
 export default EmberRemarkableComponent.extend({
     /*image: Ember.computed('imageId', function() {
-        let self = this;
-        if(this.get('imageId') === undefined || this.get('imageId') === 0) {
-            this.set('image', null);
-            return;
-        }
-        this.get('targetObject.store').findRecord('image', this.get('imageId')).then(function(image) {
-            self.set('image', image);
+      let self = this;
+      if(this.get('imageId') === undefined || this.get('imageId') === 0) {
+      this.set('image', null);
+      return;
+      }
+      this.get('targetObject.store').findRecord('image', this.get('imageId')).then(function(image) {
+      self.set('image', image);
+      });
+      }),*/
+    parsedMarkdownUnsafe: computed('text', 'html', 'typographer', 'linkify', function() {
+        var md = new Remarkable({
+            typographer: this.get('typographer'),
+            linkify:     this.get('linkify'),
+            html:        this.get('html'),
+
+            highlight: function (str, lang) {
+                if (lang === 'text' || lang === 'no-highlight') {
+                    return '';
+                }
+
+                if (lang && hljs.getLanguage(lang)) {
+                    try {
+                        return hljs.highlight(lang, str).value;
+                    } catch (err) {
+                    }
+                }
+
+                try {
+                    return hljs.highlightAuto(str).value;
+                } catch (err) {
+                }
+
+                return '';
+            }
         });
-    }),*/
+
+        if (this.get('extensions')) {
+            md.core.ruler.enable([
+                    'abbr'
+            ]);
+            md.block.ruler.enable([
+                    'footnote',
+                    'deflist'
+            ]);
+            md.inline.ruler.enable([
+                    'footnote_inline',
+                    'ins',
+                    'mark',
+                    'sub',
+                    'sup'
+            ]);
+        }
+        //overwrite the footnote rules with what we will need:
+        let location = window.location.href;
+        location = location.replace(/#.*?$/, "");
+        md.renderer.rules.footnote_ref = function (tokens, idx) {
+            var n = Number(tokens[idx].id + 1).toString();
+            var id = 'fnref' + n;
+            if (tokens[idx].subId > 0) {
+                id += ':' + tokens[idx].subId;
+            }
+            return '<sup class="footnote-ref"><a href="' + location + '#fn' + n + '" id="' + id + '">[' + n + ']</a></sup>';
+        };
+
+        md.renderer.rules.footnote_anchor = function (tokens, idx) {
+            var n = Number(tokens[idx].id + 1).toString();
+            var id = 'fnref' + n;
+            if (tokens[idx].subId > 0) {
+                id += ':' + tokens[idx].subId;
+            }
+            return ' <a href="' + location + '#' + id + '" class="footnote-backref">↩</a>';
+        };
+
+        return md.render(this.get('text'));
+    }),
     parsedMarkdownCites: computed('parsedMarkdownUnsafe', 'image', function () {
         let parsedMarkdown = this.get('parsedMarkdownUnsafe');
         let image = this.get('image');
@@ -31,6 +98,12 @@ export default EmberRemarkableComponent.extend({
             end: e.selectionEnd
         };
     },
+setSelectionRange: function(selectionStart, selectionEnd) {
+        let e = Ember.$('#' + this.get('elementId')).find("textarea");
+        e = e[0];
+    e.focus();
+    e.setSelectionRange(selectionStart, selectionEnd);
+},
     decorate: function(prepend, append) {
         let text = this.get('text');
         let selection = this.getSelection();
@@ -39,6 +112,15 @@ export default EmberRemarkableComponent.extend({
         let text_3 = text.substring(selection.end, text.length);
         this.set('text', text_1+text_2+text_3);
     },
+    compEdit: computed('edit', 'innerEdit', function() {
+        let edit = this.get('edit');
+        let innerEdit = this.get('innerEdit');
+        if(innerEdit === undefined || innerEdit === null) {
+            return edit;
+        } else {
+            return innerEdit;
+        }
+    }),
     actions: {
         bold: function() {
             const bold = "__";
@@ -52,7 +134,12 @@ export default EmberRemarkableComponent.extend({
             this.decorate("### ", "\n");
         },
         preview: function() {
-            this.toggleProperty('edit');
+            let innerEdit = this.get('innerEdit');
+            if(innerEdit === undefined || innerEdit === null) {
+                this.set('innerEdit', !this.get('edit'));
+            } else {
+                this.toggleProperty('innerEdit');
+            }
         },
         selectImage: function() {
             let deferred = this.attrs.selectImage();
@@ -62,6 +149,15 @@ export default EmberRemarkableComponent.extend({
             }, function(reason) {
                 console.log(reason);
             });
+        },
+        addFootnote: function () {
+            let text = this.get('text');
+            let selection = this.getSelection();
+            let id = text.substring(selection.start, selection.end);
+            this.decorate('[^', ']');
+            this.set('text', this.get('text') + '\n\n[^'+id+']: ');
+            let endPos = this.get('text.length');
+            this.setSelectionRange(endPos, endPos);
         }
     }
 });
